d92fda9b69d26e4b5b323d150cdd69c90d7d44a0
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2026-08-07 20:31:38 -0400

    add detailed implementation and comments for pacificAtlantic function, including DFS traversal logic and complexity analysis

--- David_Martinez/2026/graph/pacificAtlanticWaterflow.cpp
+++ David_Martinez/2026/graph/pacificAtlanticWaterflow.cpp
@@ -153,3 +153,130 @@ public:
 
         // times up
 };
+
+/*
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        
+        // input
+        //     - heights matrix representing heights above sea level in an island
+
+        // output
+        //     - return vector of coordinates of cells were if it rains we can make it
+        //         to both oceans
+        //     - we can make it to both oceans when all the cells until the ocean are less 
+        //         or equal to the current cell.
+
+
+        // constraints
+        //     - range of sizes of m and n? [1, 200]
+        //     - range of heights each cell can have? [0, 100k]
+
+        // example
+        //     heights = [
+        //         [1,2,2,3,5],
+        //         [3,2,3,4,4],
+        //         [2,4,5,3,1],
+        //         [6,7,1,4,5],
+        //         [5,1,1,2,4]]
+
+        // brute force
+        //     the nature of the problem indicates that we have to do a dfs traversal
+        //     we could traverse each cell and do a dfs to see if we get to both oceans
+        //         if the next cell is greater, we stop that traversal
+        //             we traverse to all 4 directions. up down, left, right
+
+        //     complexity: 
+        //         runtime: o(n*m)^2
+        //         memory: o(n*m) stack calls
+
+        // optimize
+        //     best conceivable runtime: o(n*m) we have to traverse all cells at least once
+
+        //     if current status depends on future statuses, its easier to start from the end
+        //         we can start from all borders and calculate to where we can get
+
+        //         we can store all possible cells we can reach from each ocean and then
+        //         return the union of those cordinates. the ones shared.
+
+        //     runtime: o(n*m)?
+        //     memory: o(n*m) if we store all coordinates
+
+        // test
+        //     - we can reach both oceans
+        //     - we cannot reach any ocean
+        
+
+        int rows = heights.size();
+        int cols = heights[0].size();
+        vector<vector<bool>> pacific(rows, vector<bool>(cols, false));
+        vector<vector<bool>> atlantic(rows, vector<bool>(cols, false));
+
+        // traverse top and bottom rows
+        for(int col = 0; col < cols; col++){
+            dfs(heights, rows, cols, pacific, 0, col, heights[0][col]);
+            dfs(heights, rows, cols, atlantic, rows-1, col, heights[rows-1][col]);
+        }
+
+        // traverse left and right border
+        for(int row = 0; row < rows; row++){
+            dfs(heights, rows, cols, pacific, row, 0, heights[row][0]);
+            dfs(heights, rows, cols, atlantic, row, cols-1, heights[row][cols-1]);
+        }
+
+        vector<vector<int>> result;
+
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                if(pacific[row][col] && atlantic[row][col]) result.push_back({row, col});
+            }
+        }
+
+        return result;
+    }
+
+
+    void dfs(
+        vector<vector<int>>& heights, 
+        int& rows,
+        int&cols,
+        vector<vector<bool>> & visited,
+        int row,
+        int col,
+        int prev
+        ){
+
+            // base cases: 
+            // rows out of bounds
+            if(row < 0 || row == rows) return;
+
+            // cols out of bounds
+            if(col < 0 || col == cols) return;
+
+            // coordinate in visited
+            if(visited[row][col]) return;
+
+            // current value less than prev
+            if(heights[row][col] < prev) return;
+
+            // mark as visited
+            visited[row][col] = true;
+
+            // visit adj cells
+            dfs(heights, rows, cols, visited, row+1, col, heights[row][col]);
+            dfs(heights, rows, cols, visited, row-1, col, heights[row][col]);
+            dfs(heights, rows, cols, visited, row, col+1, heights[row][col]);
+            dfs(heights, rows, cols, visited, row, col-1, heights[row][col]);
+        }
+};
+complexity 
+    runtime: o(n*m)
+    memory; o(n*m)
+
+I did this problem yesterrday, but its good to remember that when my current status depends
+on a future status. It´s better to start from the end and walk backwards.
+
+
+
+*/
